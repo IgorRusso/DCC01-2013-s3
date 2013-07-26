d502742b320b15775ba4bf5eaf0b4b97856467e0
IgorRusso <igor.souza.russo@gmail.com>
2013-07-26 19:10:40 -0300

    Solução recursiva para binômio de newton

--- binomio.py
+++ binomio.py
@@ -7,15 +7,12 @@ def binomioRecursive(n,k):
     t2 = binomio(n-1,k)
     return t1+t2
 
-def binomioDP(n,k):
-    
-    matrix = [[0 for i in xrange(n+1)] for i in xrange(n+1)]   
-
+def binomioDP(n,k):    
+    matrix = [[0 for i in xrange(n+1)] for i in xrange(n+1)]
     for i in range(0,n+1):
         for j in range(0,k+1):
             if i==j or j==0:
                 matrix[i][j] = 1
             else:
-                matrix[i][j] = matrix[i-1][j-1] + matrix[i-1][j]          
-
+                matrix[i][j] = matrix[i-1][j-1] + matrix[i-1][j]
     return matrix[n][k]
